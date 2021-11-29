9040e07111c12adcf4a6734a82ac34ded19f474e
WaifuForever <itachiut1r4@gmail.com>
2021-11-29 19:43:28 -0300

    add: postgres and docker setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRE_HOST,
+      port: parseInt(<string>process.env.POSTGRE_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true, //do not use it in production
+      retryDelay: 2000,
+      retryAttempts: 2,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/scripts/01-init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+export PGPASSWORD=$POSTGRES_PASSWORD;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  
+  CREATE DATABASE docker;
+  GRANT ALL PRIVILEGES ON DATABASE docker TO $POSTGRES_USER;
+  
+EOSQL
