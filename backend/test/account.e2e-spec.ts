1a03eab4e3a44e24f1333d225f546ab69bbcad51
WaifuForever <itachiut1r4@gmail.com>
2021-12-13 12:23:13 -0300

    update: packages && e2e tests

--- backend/test/account.e2e-spec.ts
+++ backend/test/account.e2e-spec.ts
@@ -1,43 +1,84 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import supertest, * as request from 'supertest';
 import { AccountModule } from '../src/models/accounts/account.module';
 import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
 import { AccountEntity } from '../src/models/accounts/entities/account.entity';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseConfig } from '../src/database/database.config';
+import { AppModule } from 'src/app.module';
+import { Repository } from 'typeorm';
+import { Account } from 'src/models/accounts/interfaces/account.interface';
+import { config } from 'dotenv';
+config();
 
-describe('AccountController (e2e)', () => {
-    let app: INestApplication;
+let app: INestApplication;
 
-    const mockAccountRepository = {};
+let repository: Repository<AccountEntity>;
 
-    beforeEach(async () => {
-        const moduleFixture: TestingModule = await Test.createTestingModule({
-            imports: [
-                ConfigModule.forRoot(),
-                TypeOrmModule.forRoot({
-                    type: 'postgres',
-                    host: process.env.POSTGRE_HOST,
-                    port: parseInt(<string>process.env.POSTGRE_PORT),
-                    username: process.env.POSTGRES_USER,
-                    password: process.env.POSTGRES_PASSWORD,
-                    database: process.env.POSTGRES_DATABASE,
-                    autoLoadEntities: true,
-                    synchronize: false, //do not set it true in production
-                    retryDelay: 2000,
-                    retryAttempts: 2,
-                }),
-                AccountModule],
-        }).overrideProvider(getRepositoryToken(AccountEntity)).useValue(mockAccountRepository).compile();
+beforeAll(async () => {
+    console.log(DatabaseConfig);
+    const module = await Test.createTestingModule({
+        imports: [
+            AccountModule,          
+            // Use the e2e_test database to run the tests
+            TypeOrmModule.forRoot(DatabaseConfig),
+        ],
+    }).compile();
+    app = module.createNestApplication();
+    await app.init();
+    repository = module.get('AccountRepository');
+});
 
-        app = moduleFixture.createNestApplication();
-        await app.init();
-    });
+afterAll(async () => {
+    await repository.query(`DELETE FROM account;`);
+    await app.close();
+});
 
-    it('/ (GET)', () => {
-        return request(app.getHttpServer())
-            .get('/')
-            .expect(200)
-            .expect('Hello World!');
+describe('GET /accounts', () => {
+    it('should return an array of accounts', async () => {
+        const account: Account = {
+            firstName: 'Jojo',
+            lastName: 'Souza',
+            password: 'asdkaXs@3123',
+            email: 'email@gmail.com',
+            birthDate: new Date('2000-06-25'),
+        };
+
+        const account2: Account = {
+            firstName: 'Kaka',
+            lastName: 'Mantega',
+            password: 'adasda@Sd3123',
+            email: 'email2@email.com',
+            birthDate: new Date('2001-02-13'),
+        };
+
+        // Pre-populate the DB with some dummy users
+        await repository.save([account, account2]);
+
+        // Run your end-to-end test
+        const { body } = await supertest
+            .agent(app.getHttpServer())
+            .get('/accounts')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200);
+
+        expect(body).toEqual([
+            {
+                id: expect.any(Number),
+                firstName: account.firstName,
+                lastName: account.lastName,
+                email: account.email,
+                birthDate: account.birthDate,
+            },
+            {
+                id: expect.any(Number),
+                firstName: account2.firstName,
+                lastName: account2.lastName,
+                email: account2.email,
+                birthDate: account2.birthDate,
+            },
+        ]);
     });
 });
--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -2,17 +2,28 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseConfig } from '../src/database/database.config';
+import { AccountModule } from '../src/models/accounts/account.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+config();
 
 describe('AppController (e2e)', () => {
     let app: INestApplication;
 
-    beforeEach(async () => {
+    beforeAll(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [AppModule],
         }).compile();
 
         app = moduleFixture.createNestApplication();
+        // app.useLogger(new TestLogger()) // more on this line is below
         await app.init();
+    });
+
+    afterAll(async () => {
+        await app.close();
     });
 
     it('/ (GET)', () => {
