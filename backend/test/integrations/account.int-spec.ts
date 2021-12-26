4e8995dd044d3f08efec62aef722b065a6c8d46c
WaifuForever <itachiut1r4@gmail.com>
2021-12-26 13:51:50 -0300

    update: integrations and e2e tests

--- backend/test/integrations/account.int-spec.ts
+++ backend/test/integrations/account.int-spec.ts
@@ -7,29 +7,33 @@ import { AccountEntity } from '../../src/models/accounts/account.entity';
 import { bootstrapTest } from '../apps/account.e2e';
 import { acc1, acc2, acc3_fake } from '../mocks/account.mock';
 
+
 let app: INestApplication;
 
-beforeAll(async () => {
+let repository: Repository<AccountEntity>;
+
+beforeAll(async () => {    
     app = await bootstrapTest();
     await app.init();
+   
 });
 
 afterAll(async () => {
+   
     await app.close();
 });
 
 describe('POST /accounts', () => {
-    it('should return the created account', async () => {
+    it('should return an array of accounts', async () => {     
         // Run your end-to-end test
-        const { body } = await request
-            .default(app.getHttpServer())
+        const { body } = await request.default(app.getHttpServer())           
             .post('/accounts')
             .send(acc1)
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200);
 
-        console.log(body);
+        console.log(body)
         expect(body).toEqual([
             {
                 id: expect.any(Number),
@@ -37,30 +41,18 @@ describe('POST /accounts', () => {
                 lastName: acc1.lastName,
                 email: acc1.email,
                 birthDate: acc1.birthDate,
-            },
+            }
         ]);
-    });
-
-    describe('when ther account already exists', () => {
-        it('should return BadRequestException', async () => {
-            const response = await request
-                .default(app.getHttpServer())
-                .post('/users')
-                .send(acc1)
-                .expect(400);
-
-            expect(response.body).toEqual(
-                'An user with this email or phone already exists.',
-            );
-        });
     });
 });
 
+
 describe('GET /accounts', () => {
     it('should return an array of accounts', async () => {
+       
+      
         // Run your end-to-end test
-        const { body } = await request
-            .default(app.getHttpServer())
+        const { body } = await request.default(app.getHttpServer())           
             .get('/accounts')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
@@ -73,7 +65,14 @@ describe('GET /accounts', () => {
                 lastName: acc1.lastName,
                 email: acc1.email,
                 birthDate: acc1.birthDate,
-            }
+            },
+            {
+                id: expect.any(Number),
+                firstName: acc2.firstName,
+                lastName: acc2.lastName,
+                email: acc2.email,
+                birthDate: acc2.birthDate,
+            },
         ]);
     });
 });
